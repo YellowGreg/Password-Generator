d8d296a8dfc20cd9711284aaf6bee9e592bf9b28
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-01-12 11:54:57 -0500

    Update pass.js

--- pass.js
+++ pass.js
@@ -1,10 +1,10 @@
 // Made By YellowGreg
 var generator = require('generate-password');
 var passwords = generator.generateMultiple(4, { // how many password you want to generate
-  
+	
 	length: 8, // How Long You want your password to be
 	uppercase: true, // if false no uppercase letters
-  numbers: true, // if false no numbers
-  symbols: true, // if false then no symbols
+	numbers: true, // if false no numbers
+	symbols: true, // if false then no symbols
 });
 console.log(passwords);
